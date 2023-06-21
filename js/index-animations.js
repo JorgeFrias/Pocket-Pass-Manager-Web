674fd047c57af073fb867d1185d62dbb84712072
Jorge Frias <jorge@reliby.com>
2023-06-21 16:38:34 +0200

    Working on animations.

---
+++ js/index-animations.js
@@ -0,0 +1,32 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+// Reveal func with JQuery
+function revealJQuery() {
+    var reveals = $(".reveal, .reveal-fast");
+    
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            $(reveals[i]).addClass("active");
+        } else {
+            $(reveals[i]).removeClass("active");
+        }
+    }
+}
+
+// Add event listener to window using revealJQuery func
+window.addEventListener("scroll", revealJQuery);
