a7c9b1c69a49b1062c8117b341d0bb7814c7ec53
Jorge Frias <jorge@reliby.com>
2023-06-22 03:05:27 +0200

    Progress.

--- js/index-animations.js
+++ js/index-animations.js
@@ -30,3 +30,51 @@ window.addEventListener("scroll", revealJQuery);
 $(document).ready(function() {
     revealJQueryAuto();
 });
+
+// Scroll horizontal
+$(document).ready(function () {
+    $(document).ready(function () {
+        var container = $('.scrolling-container');
+        var content = $('.scrolling-content');
+        var isHorizontalScrolling = false;
+
+        contentWidth = content.outerWidth() + parseInt(content.css('margin-left')) + parseInt(content.css('margin-right')) + parseInt(content.css('padding-left')) + parseInt(content.css('padding-right') + parseInt(container.css('padding-left')) + parseInt(container.css('padding-right')));
+
+        container.on('scroll', function (e) {
+            if (isHorizontalScrolling) {
+                console.log('horizontal scrolling');
+                e.preventDefault();
+                return false;
+            }
+
+            // Check if the horizontal scroll has reached the end
+            if (container.scrollLeft() >= contentWidth) {
+                // Allow vertical scrolling when the end is reached
+                console.log('end reached. \t Status: scrollLeft: ' + container.scrollLeft() + ' innerWidth: ' + container.innerWidth() + ' outerWidth: ' + outerWidth);
+                isHorizontalScrolling = false;
+                e.preventDefault();
+                // e.stopPropagation();
+                // e.stopImmediatePropagation();
+            }
+        });
+
+        container.on('wheel', function (e) {
+            if (container.scrollLeft() >= contentWidth) {
+                return;
+            }
+
+            if (container.is(e.target) || container.has(e.target).length > 0) {
+                var delta = e.originalEvent.deltaY;
+                var scrollAmount = delta * 1;
+
+                container.scrollLeft(container.scrollLeft() + scrollAmount);
+                e.preventDefault();
+                isHorizontalScrolling = true;
+
+                setTimeout(function () {
+                    isHorizontalScrolling = false;
+                }, 100);
+            }
+        });
+    });
+});
